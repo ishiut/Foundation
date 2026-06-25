e879d1ab469fd6247b78864cc6a31f088e3be6b9
Tetsuya Ishiu <ishiut@miamioh.edu>
2026-06-25 04:29:36 +0000

    Jun 25, 2026, 12:29 AM [skip ci]

--- Foundation/FirstOrder/SetTheory/test/test.lean
+++ Foundation/FirstOrder/SetTheory/test/test.lean
@@ -1,16 +1,28 @@
 module
 
-public import Foundation.FirstOrder.SetTheory.Basic.Axioms
+public import Foundation.FirstOrder.SetTheory.Basic
+public import Foundation.FirstOrder.Basic.Semantics.Semantics
+public import Foundation.FirstOrder.Basic.Syntax.Formula
 
 namespace LO.FirstOrder.SetTheory
+namespace Semiformula
 
 variable {V : Type*} [SetStructure V]
+variable (a : V)
 
-scoped instance : HasSubset V := ⟨fun x y ↦ ∀ z ∈ x, z ∈ y⟩
+-- scoped instance : HasSubset V := ⟨fun x y ↦ ∀ z ∈ x, z ∈ y⟩
 
 #check V
-#check ∀ (a b : V), a ⊆ b
+#check IsEmpty
+#check IsEmpty a
+#check “∃ e, ∀ y, y ∉ e”
+
+def empty : Sentence ℒₛₑₜ := “∃ e, ∀ y, y ∉ e”
+
+def infinity : Sentence ℒₛₑₜ := “∃ I, (∀ e, !isEmpty e → e ∈ I) ∧ (∀ x ∈ I, ∀ x', !isSucc x' x → x' ∈ I)”
+
 
 lemma subset_def {a b : V} : a ⊆ b ↔ ∀ x ∈ a, x ∈ b := by rfl
 
+end Semiformula
 end LO.FirstOrder.SetTheory
