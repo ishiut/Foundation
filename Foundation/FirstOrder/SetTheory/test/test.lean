0ef31432cb92eb22c93606a178e874ef416ecbe3
Tetsuya Ishiu <ishiut@miamioh.edu>
2026-06-25 03:55:21 +0000

    Jun 24, 2026, 11:55 PM [skip ci]

--- Foundation/FirstOrder/SetTheory/test/test.lean
+++ Foundation/FirstOrder/SetTheory/test/test.lean
@@ -6,6 +6,11 @@ namespace LO.FirstOrder.SetTheory
 
 variable {V : Type*} [SetStructure V]
 
+scoped instance : HasSubset V := ⟨fun x y ↦ ∀ z ∈ x, z ∈ y⟩
+
 #check V
+#check ∀ (a b : V), a ⊆ b
+
+lemma subset_def {a b : V} : a ⊆ b ↔ ∀ x ∈ a, x ∈ b := by rfl
 
 end LO.FirstOrder.SetTheory
