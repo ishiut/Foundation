72eacc8be05eed97205e8dd746b678ed1d6faa15
Tetsuya Ishiu <ishiut@miamioh.edu>
2026-06-25 03:48:18 +0000

    Jun 24, 2026, 11:48 PM [skip ci]

--- Foundation/FirstOrder/SetTheory/test/test.lean
+++ Foundation/FirstOrder/SetTheory/test/test.lean
@@ -2,4 +2,10 @@ module
 
 public import Foundation.FirstOrder.SetTheory.Basic.Axioms
 
+namespace LO.FirstOrder.SetTheory
 
+variable {V : Type*} [SetStructure V]
+
+#check V
+
+end LO.FirstOrder.SetTheory
